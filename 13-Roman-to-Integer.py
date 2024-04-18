0e32080510dd68ce729924ac456628f87ba9c0ad
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-04-18 18:54:08 -0400

    Update 13

--- 13-Roman-to-Integer.py
+++ 13-Roman-to-Integer.py
@@ -13,18 +13,10 @@ def romanNumber(roman):
 
     for number in roman:
         temp = diccionary[number]
-        print("NUMERO DICCIONARIO ", diccionary[number])
-        print("NUMERO TEMP", temp)
-        print("RESULTADO: ", result)
-        print("EXTERIOR IF: ", temp * 3)
-        print()
         if temp * 3 > result:
-            # print("INTERIOR IF", temp * 3, result)
             result += temp
-            print("RESULTADO DESPUES POSITIVO", result)
         else:
             result -= temp
-            print("RESULTADO DESPUES NEGATIVO", result)
     return result
 
 
