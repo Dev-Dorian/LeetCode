547bc9f48632b86566635dbcfc879fd7978a72bc
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-05-25 20:32:29 -0400

    Exercise 7

--- 07-Reverse-Integer.py
+++ 07-Reverse-Integer.py
@@ -1,16 +1,19 @@
-def reverseInteger(x):
-    result = 0
-    prev = 0
+class Solution:
+    def reverseInteger(self, x):
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        result = 0
 
-    while (x != 0):
-        cur = x % 10
-        x //= 10
-        result = result * 10 + cur
-        if ((result - cur) // 10 != prev):
+        while (x > 0):
+            cur = x % 10
+            result = result * 10 + cur
+            x //= 10
+        result *= sign
+
+        if result < -2**31 or result > 2**31 - 1:
             return 0
-        prev = result
-    return result
+        return result
 
 
 if __name__ == "__main__":
-    print(reverseInteger(123))
+    print(Solution().reverseInteger(-123))
