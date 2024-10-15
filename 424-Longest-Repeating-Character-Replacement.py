5067d05c2a0e4b3da5fc622c7cb4170d81eb7d96
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-10-15 19:08:06 -0400

    Update Challenge

--- 424-Longest-Repeating-Character-Replacement.py
+++ 424-Longest-Repeating-Character-Replacement.py
@@ -1,3 +1,5 @@
+# exercise using Sliding Window
+
 def characterReplacement(s, k):
     longest = 0
     left = 0
@@ -10,8 +12,28 @@ def characterReplacement(s, k):
         longest = max(longest, (right-left+1))
     return longest
 
+# exercise using Sliding Window
+
+
+def characterReplacement1(s, k):
+    count = {}
+    maxLength = 0
+    start = 0
+    end = 0
+    maxC = 0
+    while end < len(s):
+        count[s[end]] = 1 + count.get(s[end], 0)
+        maxC = max(maxC, count[s[end]])
+        while (end - start + 1) - maxC > k:
+            count[s[start]] -= 1
+            start += 1
+        maxLength = max(maxLength, (end - start + 1))
+        end += 1
+    return maxLength
+
 
 s = "ABAB"
 k = 2
 
 print(characterReplacement(s, k))
+print(characterReplacement1(s, k))
