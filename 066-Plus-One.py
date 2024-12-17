ddec75af2b72cf67c603470eb934cf7a61c2f559
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-12-16 19:50:25 -0500

    Update Challenge

--- 066-Plus-One.py
+++ 066-Plus-One.py
@@ -1,15 +1,11 @@
 def plusOne(digits):
-    listLigits = ""
-    for i in range(len(digits)):
-        listLigits += str(digits[i])
-        print(listLigits)
-    num = int(listLigits) + 1
-    listLigits = str(num)
-    print(listLigits)
-    digits.clear()
-    for j in range(len(listLigits)):
-        digits.append(int(listLigits[j]))
-    return digits
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] = digits[i] + 1
+            return digits
+    return [1] + digits
 
 
 digits = [1, 2, 3]
