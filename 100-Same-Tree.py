be72ec33f353a60e66a3e5628bd7c6243379c930
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-12-20 19:42:59 -0500

    Upload

--- 100-Same-Tree.py
+++ 100-Same-Tree.py
@@ -24,8 +24,19 @@ class Solution:
 
         # return False
 
+    def isSameTree1(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+
+        return (self.isSameTree1(p.left, q.left) and self.isSameTree1(p.right, q.right))
+
 
 if __name__ == "__main__":
     root1, root1.left, root1.right = TreeNode(1), TreeNode(2), TreeNode(3)
     root2, root2.left, root2.right = TreeNode(1), TreeNode(2), TreeNode(3)
     print(Solution().isSame(root1, root2))
+    print(Solution().isSameTree1(root1, root2))
