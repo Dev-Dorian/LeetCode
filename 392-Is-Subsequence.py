76227aac30a816c2108a1eef4636c43b0c3ab1d0
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-01-21 19:41:35 -0500

    Update Challenge

--- 392-Is-Subsequence.py
+++ 392-Is-Subsequence.py
@@ -14,7 +14,18 @@ def isSubsequence(s, t):
     return False
 
 
+def isSubsequence_1(s, t):
+    i = 0
+    j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return True if i == len(s) else False
+
+
 s = "abc"
 t = "ahbgd"
 
 print(isSubsequence(s, t))
+print(isSubsequence_1(s, t))
