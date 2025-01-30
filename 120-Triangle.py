0043261535e18ef0b05d05fbfc6dde441e43e08f
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-01-30 16:03:24 -0500

    Update

--- 120-Triangle.py
+++ 120-Triangle.py
@@ -1,10 +1,11 @@
 def minimumTotal(triangle):
     for row in range(len(triangle)-2, -1, -1):
         for col in range(0, len(triangle[row])):
+            # print((triangle[row+1][col], triangle[row+1][col+1]))
             triangle[row][col] += min(triangle[row+1]
                                       [col], triangle[row+1][col+1])
     return triangle[0][0]
 
 
-triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
+triangle = [[2], [3, 4], [6, 5, 7]]
 print(minimumTotal(triangle))
