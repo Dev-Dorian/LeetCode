1653e36b22054a900082834a8ab84340d3f06c61
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-02-10 19:20:41 -0500

    Update

--- 220-Contains-Duplicate-III.py
+++ 220-Contains-Duplicate-III.py
@@ -1,3 +1,6 @@
+import bisect
+
+
 def containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff):
     if valueDiff == 0 and len(set(nums)) == len(nums):
         return False
@@ -6,6 +9,7 @@ def containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff):
     width = valueDiff + 1
 
     for i, n in enumerate(nums):
+        # print(n // width)
         bucket_i = n // width
 
         if bucket_i in bucket:
@@ -22,7 +26,36 @@ def containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff):
     return False
 
 
-nums = [1, 2, 3, 1]
-indexDiff = 3
-valueDiff = 0
+nums = [1, 2, 3, 1]  # [8,7,15,1,6,1,9,15]
+indexDiff = 3  # 1
+valueDiff = 0  # 3
 print(containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff))
+
+
+def containsNearbyAlmostDuplicate1(nums, indexDiff, valueDiff):
+    window = []
+    for i in range(len(nums)):
+        if i > indexDiff:
+            # Remove the element that is no longer in the window
+            val_to_remove = nums[i - indexDiff - 1]
+            index = bisect.bisect_left(window, val_to_remove)
+            if index < len(window) and window[index] == val_to_remove:
+                del window[index]
+
+        # Find the position to insert nums[i]
+        pos = bisect.bisect_left(window, nums[i])
+
+        # Check the previous element
+        if pos > 0:
+            if nums[i] - window[pos - 1] <= valueDiff:
+                return True
+
+        # Check the current element
+        if pos < len(window):
+            if window[pos] - nums[i] <= valueDiff:
+                return True
+
+        # Insert nums[i] into the window
+        bisect.insort(window, nums[i])
+
+    return False
