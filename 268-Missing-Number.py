1622a7086a86ac3446668d997465a4fdf64452fc
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-02-11 12:24:30 -0500

    Same exercise second way to do it

--- 268-Missing-Number.py
+++ 268-Missing-Number.py
@@ -3,9 +3,18 @@ def missingNumber(nums):
 
     for i in range(len(nums)):
         res ^= i ^ nums[i]
-        print(res)
+        # print(res)
     return res
 
 
+def missingNumber1(nums):
+    n = len(nums)
+    expectedSum = n*(n+1)//2
+    print(expectedSum)
+    actualSum = sum(nums)
+    return expectedSum - actualSum
+
+
 nums = [3, 0, 1]
-print(missingNumber(nums))
+# print(missingNumber(nums))
+print(missingNumber1(nums))
