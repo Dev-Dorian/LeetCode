af2e29f2c6b48d3c5bcf669cbda94f5b9a3fa703
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-05-02 09:34:03 -0400

    Upload Challenge

---
+++ 134-Gas-Station.py
@@ -0,0 +1,17 @@
+def canCompleteCircuit(gas, cost):
+    start = 0
+    currentGas = 0
+    totalGas = 0
+
+    for index in range(len(gas)):
+        currentGas += gas[index] - cost[index]
+        totalGas += gas[index] - cost[index]
+        if currentGas < 0:
+            currentGas = 0
+            start = index + 1
+    return start if totalGas >= 0 else -1
+
+
+gas = [1, 2, 3, 4, 5]
+cost = [3, 4, 5, 1, 2]
+print(canCompleteCircuit(gas, cost))
