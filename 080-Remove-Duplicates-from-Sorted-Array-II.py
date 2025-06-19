871d1ec782fa2983229ebb94191cfbdc3626adfa
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-06-18 22:59:15 -0400

    Upload Challenge

--- 080-Remove-Duplicates-from-Sorted-Array-II.py
+++ 080-Remove-Duplicates-from-Sorted-Array-II.py
@@ -3,11 +3,12 @@ def removeDuplicates(nums):
     if len(nums) == 0:
         return 0
     for index in range(1, len(nums)):
+        print(nums[index], nums[count - 2])
         if count == 1 or nums[index] != nums[count - 2]:
             nums[count] = nums[index]
             count += 1
     return count
 
 
-nums = [1, 1, 1, 2, 2, 37]
+nums = [9, 9, 10, 11, 11, 15]
 print(removeDuplicates(nums))
