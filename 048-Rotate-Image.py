e269960bcb8ad8343dbe7fe1e1d6be23bae6b921
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-07-15 21:59:56 -0400

    Upload Challenge

--- 048-Rotate-Image.py
+++ 048-Rotate-Image.py
@@ -17,5 +17,17 @@ def rotate(matrix):
     return matrix
 
 
+def rotate1(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i+1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()
+    return matrix
+
+
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(rotate(matrix))
+print(rotate1(matrix))
