0c2fa8a3deca967bc339ab172e51c4924d3931e2
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-08-23 22:10:35 -0400

    Update Challenge

--- 383-Ransom-Note.py
+++ 383-Ransom-Note.py
@@ -4,12 +4,13 @@ from collections import Counter
 def canConstruct(ransomNote, magazine):
     word_1 = Counter(ransomNote)
     word_2 = Counter(magazine)
-    if word_1 == word_2:
-        return True
-    return False
+    for index in word_1:
+        if index not in word_2 or word_1[index] > word_2[index]:
+            return False
+    return True
 
 
 ransomNote = "aa"
-magazine = "aa"
+magazine = "ab"
 
 print(canConstruct(ransomNote, magazine))
