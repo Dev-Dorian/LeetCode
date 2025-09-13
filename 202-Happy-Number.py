b395ec61cd75f61e549f4915cdd5e248da78d1aa
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-09-12 22:52:34 -0400

    Upload Challenge

---
+++ 202-Happy-Number.py
@@ -0,0 +1,18 @@
+def isHappy(n):
+    seen = set()
+    cur = str(n)
+
+    while cur not in seen:
+        seen.add(cur)
+        sum = 0
+        for digit in cur:
+            digit = int(digit)
+            sum += digit ** 2
+        if sum == 1:
+            return True
+        cur = str(sum)
+    return False
+
+
+n = 2
+print(isHappy(n))
