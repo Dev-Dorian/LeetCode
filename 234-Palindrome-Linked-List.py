d1008dea2e88d337bdaf56f3b9360e38106cf1d3
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-09-15 20:00:51 -0400

    Update Challenge

--- 234-Palindrome-Linked-List.py
+++ 234-Palindrome-Linked-List.py
@@ -29,9 +29,24 @@ class Solution(object):
         return True
 
 
+class Solution_1(object):
+    def isPalindrome_1(self, head: ListNode) -> bool:
+        ans = []
+        while head:
+            ans.append(head.val)
+            head = head.next
+        i, j = 0, len(ans) - 1
+        while i < j:
+            if ans[i] != ans[j]:
+                return False
+            i, j = i + 1, j - 1
+        return True
+
+
 if __name__ == "__main__":
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = ListNode(2)
     head.next.next.next = ListNode(1)
     print(Solution().isPalindrome(head))
+    print(Solution_1().isPalindrome_1(head))
