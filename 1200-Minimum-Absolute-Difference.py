bf081372f36726b92e9e6103ecb4d7fe64683a14
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-11-22 22:44:36 -0500

    Upload Challenge

--- 1200-Minimum-Absolute-Difference.py
+++ 1200-Minimum-Absolute-Difference.py
@@ -6,8 +6,15 @@ def minimumAbsDifference(arr):
     m = math.inf
     result = []
 
-    for array in range(len(arr)):
-        print(arr[array], arr[array])
+    for array in range(1, len(arr)):
+        if arr[array] - arr[array - 1] < m:
+            m = arr[array] - arr[array - 1]
+            result = [[arr[array - 1], arr[array]]]
+        elif arr[array] - arr[array - 1] == m:
+            result.append([arr[array - 1], arr[array]])
+
+    return result
+#    print(arr[array], arr[array - 1])
 
 
 arr = [4, 2, 1, 3]
