9b36e96879437de1eed8444fbb93e8ea5f619abe
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-12-08 14:02:13 -0500

    Update Challenge

--- 155-Min-Stack.py
+++ 155-Min-Stack.py
@@ -36,15 +36,12 @@ class Solution:
 
 if __name__ == "__main__":
     stack = Solution()
-    stack.push(-2)
-    stack.push(0)
-    stack.push(3)
-    stack.push(100)
-    stack.push(-10)
-    stack.push(-5)
-    stack.push(1)
-    stack.push(40)
-    stack.pop()
-    stack.pop()
-    # stack.pop()
-    print([stack.top(), stack.getMin()])
+    print([
+        stack.__init__(),
+        stack.push(-2),
+        stack.push(0),
+        stack.push(-3),
+        stack.getMin(),
+        stack.pop(),
+        stack.top(),
+        stack.getMin()])
--- 739-Daily-Temperatures.py
+++ 739-Daily-Temperatures.py
@@ -4,6 +4,7 @@ def dailyTemperatures(temperatures):
     for i, temp in enumerate(temperatures):
         while stack and temperatures[stack[-1]] < temp:
             prev_index = stack.pop()
+            print(i, prev_index)
             answer[prev_index] = i - prev_index
 
         stack.append(i)
