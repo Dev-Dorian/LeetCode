6176551495acc81ecc2b60b7870b71b4c9640478
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-12-18 22:15:14 -0500

    Uplaod Challenge

--- 232-Implement-Queue-using-Stacks.py
+++ 232-Implement-Queue-using-Stacks.py
@@ -1,27 +1,50 @@
 class MyQueue(object):
 
     def __init__(self):
+        self.stack_1 = []
+        self.stack_2 = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
+        while self.stack_1:
+            self.stack_2.append(self.stack_1.pop())
+        self.stack_1.append(x)
+
+        while self.stack_2:
+            self.stack_1.append(self.stack_2.pop())
 
     def pop(self):
         """
         :rtype: int
         """
+        return self.stack_1.pop()
 
     def peek(self):
         """
         :rtype: int
         """
+        return self.stack_1[-1]
 
     def empty(self):
         """
         :rtype: bool
         """
+        return not self.stack_1
+
+
+if __name__ == "__main__":
+    stack = MyQueue()
+    print([
+        stack.__init__(),
+        stack.push(1),
+        stack.push(2),
+        stack.peek(),
+        stack.pop(),
+        stack.empty()
+    ])
 
 
 # Your MyQueue object will be instantiated and called as such:
