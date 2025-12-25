4064268053939d32b7d3551c712e20cbc0b25e03
Dorian Hidalgo <dorianhl5229@gmail.com>
2025-12-24 20:32:20 -0500

    Update Challenge

--- 496-Next-Greater-Element-I.py
+++ 496-Next-Greater-Element-I.py
@@ -7,7 +7,16 @@ def nextGreaterElement(nums1, nums2):
             last_element = stack.pop()
             next_greater[last_element] = num
         stack.append(num)
-    return [next_greater.get(n, -1) for n in nums1]
+
+    final_list = []
+    for n in nums1:
+        if n in next_greater:
+            val = next_greater[n]
+        else:
+            val = -1
+        final_list.append(val)
+    return final_list
+    # return [next_greater.get(n, -1) for n in nums1]
 
 
 nums1 = [4, 1, 2]
