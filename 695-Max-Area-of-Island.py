b3a56f6e37df0c1dec6959f79a8b18288e4f2255
Dorian Hidalgo <dorianhl5229@gmail.com>
2026-01-09 17:09:20 -0600

    Upload Challenge

---
+++ 695-Max-Area-of-Island.py
@@ -0,0 +1,31 @@
+def maxAreaOfIsland(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def dfs(r, c):
+        if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
+            return 0
+
+        grid[r][c] = 0
+        area = 1 + dfs(r + 1, c) + dfs(r - 1, c) + \
+            dfs(r, c + 1) + dfs(r, c - 1)
+
+        return area
+    max_area = 0
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                max_area = max(max_area, dfs(r, c))
+    return max_area
+
+
+grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+
+print(maxAreaOfIsland(grid))
