150fbe7f79a9a107de296ff636de8e5030c9037b
Dorian Hidalgo <dorianhl5229@gmail.com>
2026-01-09 23:36:58 -0600

    Update Code

--- 463-Island-Perimeter.py
+++ 463-Island-Perimeter.py
@@ -1,4 +1,13 @@
 def islandPerimeter(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def dfs(r, c):
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c]:
+                return dfs(r, c)
 
 
 grid = [[0, 1, 0, 0],
