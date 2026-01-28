b892c56e022a84ba519c57beab495622592906e5
Dorian Hidalgo <dorianhl5229@gmail.com>
2026-01-27 21:29:32 -0500

    Code Update

--- 661-Image-Smoother.py
+++ 661-Image-Smoother.py
@@ -4,6 +4,19 @@ def imageSmoother(img):
 
     res = [[0] * cols for _ in range(rows)]
 
+    for r in range(rows):
+        for c in range(cols):
+            total_sum = 0
+            count = 0
+
+            for i in range(r-1, r+2):
+                for j in range(c-1, c+2):
+                    if 0 <= i < rows and 0 <= j < cols:
+                        total_sum += img[i][j]
+                        count += 1
+            res[r][c] = total_sum // count
+    return res
+
 
 img = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
 print(imageSmoother(img))
