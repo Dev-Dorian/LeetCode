03c445569dc67ab8142d165afbb7a2a431bae6e8
Dorian Hidalgo <dorianhl5229@gmail.com>
2026-02-02 22:16:20 -0500

    Upload Code

--- 063-Unique-Paths-II.py
+++ 063-Unique-Paths-II.py
@@ -1,7 +1,15 @@
 def uniquePathsWithObstacles(obstacleGrid):
     M, N = len(obstacleGrid), len(obstacleGrid[0])
     dp = {(M - 1, N - 1): 1}
-    print(dp)
+
+    def dfs(row, col):
+        if row == M or col == N or obstacleGrid[row][col]:
+            return 0
+        if (row, col) in dp:
+            return dp[(row, col)]
+        dp[(row, col)] = dfs(row + 1, col) + dfs(row, col + 1)
+        return dp[(row, col)]
+    return dfs(0, 0)
 
 
 obstacleGrid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
